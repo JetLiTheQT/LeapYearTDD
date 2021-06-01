2c061c42a215e59b11cea9c4bfe32e68061871f1
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-31 22:02:50 -0700

    Implemented code that returns leap year if the input number is divisible by 400 in the case that it's divisble by 100

--- LeapYear.py
+++ LeapYear.py
@@ -3,5 +3,7 @@ def leap(a):
         if(a%100 != 0 ):
             return "Leap Year"
         else:
+            if(a%400==0):
+                return "Leap Year"
             return "Not Leap Year"
     
